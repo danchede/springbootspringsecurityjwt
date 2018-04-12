9bfc92fd1e76a148de42d2b0a790d02f31a2ff01
danchede <1320030382@qq.com>
2018-04-12 16:29:49 +0800

    SpringBoot-SpringSecurity-jwt完成基本环境配置

---
+++ src/main/java/com/ifox/springbootspringsecurityjwt/SpringbootspringsecurityjwtApplication.java
@@ -0,0 +1,12 @@
+package com.ifox.springbootspringsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootspringsecurityjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootspringsecurityjwtApplication.class, args);
+	}
+}
